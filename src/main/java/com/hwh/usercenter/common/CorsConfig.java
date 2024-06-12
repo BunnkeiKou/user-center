8dff61581e5d9330653f7a2edc517596ba0081ed
BunnkeiKou <hong.wenhui233@gmail.com>
2024-06-12 23:20:55 +0800

    add CorsConfig

---
+++ src/main/java/com/hwh/usercenter/common/CorsConfig.java
@@ -0,0 +1,17 @@
+package com.hwh.usercenter.common;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowCredentials(true)
+                .allowedHeaders("*");
+    }
+
+}
